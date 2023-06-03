6a717ad74fe763a6aeb35d143f6153e6f9896b57
Alisia Tusu <alisiatusu@gmail.com>
2023-06-03 17:06:10 +0300

    my sokets are working finally :>

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World from client!");
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client2
+{
+    static void Main()
+    {
+        string ipAddress = "127.0.0.1";
+        int port = 8080;
+
+        TcpClient client = new TcpClient();
+        client.Connect(ipAddress, port);
+        Console.WriteLine("Connected to server.");
+
+        NetworkStream stream = client.GetStream();
+
+        while (true)
+        {
+            // Read input from the user
+            Console.Write("Enter a message: ");
+            string message = Console.ReadLine();
+
+            // Convert the message to bytes
+            byte[] data = Encoding.ASCII.GetBytes(message);
+
+            // Send the message to the server
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Message sent: " + message);
+        }
+
+        client.Close();
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World from server!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        // Set the IP address and port for the server
+        string ipAddress = "127.0.0.1";
+        int port = 8080;
+
+        // Create a TCP listener
+        TcpListener listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+        listener.Start();
+        Console.WriteLine("Server started. Waiting for connections...");
+
+        while(true)
+        {
+        // Accept client connections
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+
+            // Get the network stream from the client
+            NetworkStream stream = client.GetStream();
+
+            // Read incoming messages
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            try
+            {
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Message received: {message}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                continue;
+            }
+
+
+            client.Close();
+        }
+        // Close the connection
+        listener.Stop();
+            
+    }
+}
