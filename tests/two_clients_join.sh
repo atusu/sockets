bbf5d44a93e022189565dd1790edd0f96bb82eb5
atusu <48450973+atusu@users.noreply.github.com>
2025-01-07 23:38:18 +0200

    Create dotnet.yml (#1)
    * Create dotnet.yml
    
    * Updated ci file
    
    * Needed to add ncat. Fixed ✨
    
    * Updated test
    
    * works
    
    * is this it
    
    * echo in file
    
    * wait n lines
    
    * 1 more fix
    
    * sleep less
    
    * hack
    
    * 1 more fix

--- tests/two_clients_join.sh
+++ tests/two_clients_join.sh
@@ -8,25 +8,42 @@ PID_SERVER=$!
 rm -f c1.txt; touch c1.txt;
 rm -f c2.txt; touch c2.txt;
 
-tail -f c1.txt | ncat.exe -v localhost 8080 > out_c1.txt &
+tail -f c1.txt | ncat -v localhost 8080 > out_c1.txt &
 PID_C1=$!
 
-tail -f c2.txt | ncat.exe -v localhost 8080 > out_c2.txt &
+tail -f c2.txt | ncat -v localhost 8080 > out_c2.txt &
 PID_C2=$!
 
-sleep 5; # first ncat takes a while for some reason
+sleep 3; # first ncat takes a while for some reason
 
 echo "-- clients connected hopefully"
 
 function cleanup_pids {
-    kill $PID_C1 $PID_C2 $PID_SERVER;
-    kill $(ps -ef | grep tail | tr -s "  " " " | cut -d " " -f3)
+    kill $PID_C1 $PID_C2 $PID_SERVER
+    kill $(ps -ef | grep tail | tr -s "  " " " | cut -d " " -f3) || echo "no such process"
+}
+
+function wait_n_lines {
+    n_tries=0
+    while true; do
+        n_lines=$(cat $1 | wc -l)
+        n_tries=$((n_tries+1))
+        if [ $n_lines -lt "$2" ]; then
+            sleep 0.01
+            continue
+        else
+            break
+        fi
+        if [ $n_tries == "1000" ]; then # stop after ~10s of waiting
+            break
+        fi
+    done
+    echo "waited $n_tries tries"
 }
 
 function test_or_die {
-    sleep 1.2; # TODO: read from $1 until number of lines increase by 1
-    test "$(cat $1 | wc -l)" == "$3" || ( echo $4; cleanup_pids; kill $$ )
-    test "$(cat $1 | tail -n 1)" == "$2" || ( echo $4; cleanup_pids; kill $$ )
+    wait_n_lines $1 $3
+    test "$(cat $1 | tail -n 1)" == "$2" || ( echo $4; echo "in file $1:"; cat $1; cleanup_pids; kill $$ )
 }
 
 echo "/join" >> c1.txt
@@ -42,7 +59,10 @@ echo "gigel" >> c2.txt
 test_or_die out_c2.txt OK 2 "failed after c2 name"
 
 echo "/get-list" >> c1.txt
-test_or_die out_c1.txt "marianela, gigel" 3 "failed after 1st get-list"
+wait_n_lines out_c1.txt 3
+test "$(cat out_c1.txt | tail -n 1)" == "marianela, gigel" || \
+    test "$(cat out_c1.txt | tail -n 1)" == "gigel, marianela" || \
+    ( echo $4; echo "in file:"; cat out_c1.txt; cleanup_pids; kill $$ )
 
 echo "/leave" >> c2.txt
 sleep 1
