dfbad3b3cf3e1f0b21518c6c4b9e70162cb2bfe8
Alisia <alisiatusu@gmail.com>
2025-05-28 20:17:41 +0300

    Halfway there :)

--- tests/Server.Tests/ServerTests.cs
+++ tests/Server.Tests/ServerTests.cs
@@ -1,3 +1,4 @@
+using System.Net.Sockets;
 using server;
 using Xunit;
 
@@ -8,5 +9,35 @@ public class ServerTests
     {
         var server = new Server(8080);
         Assert.True(server.clients.Count() == 0);
-     }
+    }
+
+    [Fact]
+    public void AddNewClient()
+    {
+        var server = new Server(8080);
+        var client1 = new MockedClient { TcpClient = null };
+        server.clients.Add(client1);
+        Assert.True(server.clients.Count == 1);
+        Assert.True(server.clients[0].ClientState == ClientState.INIT);
+        var response = client1.Write(server, "lala");
+        Assert.True(response == "ERR: you cannot join server using this command.");
+        Assert.True(server.clients[0].ClientState == ClientState.INIT);
+        response = client1.Write(server, "/join");
+        Assert.True(response == "OK");
+        Assert.True(server.clients[0].ClientState == ClientState.JOINED);
+        response = client1.Write(server, "Marinela");
+        Assert.True(response == "OK");
+        Assert.True(server.clients[0].ClientState == ClientState.CONNECTED);
+    }
+
+    public class MockedClient : ClientData
+    {
+        public string Write(Server server, string message)
+        {
+            //TcpClient.WriteMessage(message);
+            server.HandleClient(this);
+            return "";
+            //return TcpClient.ReadMessage();
+        }
+    }
 }
