f8cde032792f2c1ca8658f3d50312464fe42f652
Alisia <alisiatusu@gmail.com>
2025-07-27 11:45:13 +0300

    updated client1 -> client

--- tests/Server.Tests/ServerTests.cs
+++ tests/Server.Tests/ServerTests.cs
@@ -15,21 +15,21 @@ public class ServerTests
     public void TestAddOneClient()
     {
         var server = new MockServer(8080);
-        var client1 = new MockClientConnection();
-        server.clients.Add(client1);
+        var client = new MockClientConnection();
+        server.clients.Add(client);
         Assert.True(server.clients.Count == 1);
         Assert.True(server.clients[0].ClientState == ClientState.INIT);
-        client1.SetInput("lala");
-        server.HandleClient(client1);
-        Assert.Equal("ERR: you cannot join server using this command.", client1.GetResponse());
+        client.SetInput("lala");
+        server.HandleClient(client);
+        Assert.Equal("ERR: you cannot join server using this command.", client.GetResponse());
         Assert.True(server.clients[0].ClientState == ClientState.INIT);
-        client1.SetInput("/join");
-        server.HandleClient(client1);
-        Assert.Equal("OK", client1.GetResponse());
+        client.SetInput("/join");
+        server.HandleClient(client);
+        Assert.Equal("OK", client.GetResponse());
         Assert.True(server.clients[0].ClientState == ClientState.JOINED);
-        client1.SetInput("Marinela");
-        server.HandleClient(client1);
-        Assert.Equal("OK", client1.GetResponse());
+        client.SetInput("Marinela");
+        server.HandleClient(client);
+        Assert.Equal("OK", client.GetResponse());
         Assert.True(server.clients[0].ClientState == ClientState.CONNECTED);
     }
 }
