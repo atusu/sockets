0601643738a085ad0c6d2b20244b2b60bd1ef308
Alisia <alisiatusu@gmail.com>
2025-07-27 12:04:44 +0300

    Bundle methods into a single one

--- tests/Server.Tests/ServerTests.cs
+++ tests/Server.Tests/ServerTests.cs
@@ -1,35 +1,35 @@
 using server;
 using Server.Tests;
-using MockServer = server.Server;
 
 public class ServerTests
 {
     [Fact]
     public void EmptyServer()
     {
-        var server = new MockServer(8080);
+        var server = new server.Server(8080);
         Assert.True(server.clients.Count() == 0);
     }
 
     [Fact]
     public void TestAddOneClient()
     {
-        var server = new MockServer(8080);
+        var server = new server.Server(8080);
         var client = new MockClientConnection();
         server.clients.Add(client);
         Assert.True(server.clients.Count == 1);
         Assert.True(server.clients[0].ClientState == ClientState.INIT);
-        client.SetInput("lala");
-        server.HandleClient(client);
-        Assert.Equal("ERR: you cannot join server using this command.", client.GetResponse());
+        Assert.Equal("ERR: you cannot join server using this command.", HandleClientCommand("lala", server, client));
         Assert.True(server.clients[0].ClientState == ClientState.INIT);
-        client.SetInput("/join");
-        server.HandleClient(client);
-        Assert.Equal("OK", client.GetResponse());
+        Assert.Equal("OK", HandleClientCommand("/join", server, client));
         Assert.True(server.clients[0].ClientState == ClientState.JOINED);
-        client.SetInput("Marinela");
-        server.HandleClient(client);
-        Assert.Equal("OK", client.GetResponse());
+        Assert.Equal("OK", HandleClientCommand("Marinela", server, client));
         Assert.True(server.clients[0].ClientState == ClientState.CONNECTED);
+    }
+
+    public string HandleClientCommand(string input, server.Server server, MockClientConnection client)
+    {
+        client.SetInput(input);
+        server.HandleClient(client);
+        return client.GetResponse();
     }
 }
